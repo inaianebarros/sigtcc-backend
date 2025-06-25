f2daed4aaa08a510d0cd7b2c4c02a18e639d96ad
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-06-25 20:13:51 -0300

    feat: Adicionar rota de consulta de professores
    Issue: TCCB-8

--- sigtcc/user/controllers.py
+++ sigtcc/user/controllers.py
@@ -1,18 +1,55 @@
-from django.http.request import HttpRequest
 from django.db import transaction
+from django.http.request import HttpRequest
 from ninja.constants import NOT_SET
 from ninja_extra import api_controller
+from ninja_extra import ControllerBase
 from ninja_extra import route
 from ninja_extra import status
 from ninja_jwt.controller import TokenBlackListController
 from ninja_jwt.controller import TokenObtainPairController
 
+from core.constants import QUERY
 from sigtcc.schemas import ReturnSchema
+from user.models import ProfessorProfile
 from user.models import User
+from user.schemas import ProfessorSchemaOut
 from user.schemas import UserSchemaIn
 
 
-@api_controller('/users', auth=NOT_SET, permissions=[], tags=['Users'])
+@api_controller('/professors', auth=NOT_SET, tags=['Professors'])
+class ProfessorController(ControllerBase):
+    @route.get(
+        '/',
+        auth=NOT_SET,
+        permissions=[],
+        response={
+            status.HTTP_200_OK: list[ProfessorSchemaOut],
+            status.HTTP_400_BAD_REQUEST: ReturnSchema,
+            status.HTTP_500_INTERNAL_SERVER_ERROR: ReturnSchema,
+        },
+    )
+    def list_professors(
+        self,
+        request: HttpRequest,
+        expertise_areas_uuids: list[str] | None = QUERY,
+        first_name: str | None = None,
+        institute_uuids: str | None = None,
+    ):
+        qs = ProfessorProfile.objects.all()
+
+        if expertise_areas_uuids:
+            qs = qs.filter(expertise_areas__uuid__in=expertise_areas_uuids)
+
+        if first_name:
+            qs = qs.filter(user__first_name__icontains=first_name)
+
+        if institute_uuids:
+            qs = qs.filter(institute__uuid=institute_uuids)
+
+        return qs.distinct().order_by('user__first_name')
+
+
+@api_controller('/users', auth=NOT_SET, tags=['Users'])
 class UserController(TokenObtainPairController, TokenBlackListController):
     @route.post(
         '/',
