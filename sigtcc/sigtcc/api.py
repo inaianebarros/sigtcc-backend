8455b421675cbe8b8d7f1a41386359fbbbc0cfe9
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-06-27 00:12:49 -0300

    feat: Adicionar rotas de solicitação de supervisão ao sistema
    Issue: TCCB-10

--- sigtcc/sigtcc/api.py
+++ sigtcc/sigtcc/api.py
@@ -3,6 +3,7 @@ from ninja_extra import NinjaExtraAPI
 
 from academic.controllers import ExpertiseAreaController
 from academic.controllers import InstituteController
+from tcc.controllers import SupervisionRequestController
 from user.controllers import ProfessorController
 from user.controllers import UserController
 
@@ -12,4 +13,5 @@ api = NinjaExtraAPI(title='SIGTCC', version='1.0.0', docs=docs)
 api.register_controllers(ExpertiseAreaController)
 api.register_controllers(InstituteController)
 api.register_controllers(ProfessorController)
+api.register_controllers(SupervisionRequestController)
 api.register_controllers(UserController)
