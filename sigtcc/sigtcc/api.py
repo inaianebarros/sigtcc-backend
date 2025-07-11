46d8dcb441ee6eab4241cce70dfa438b7b66bb90
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-07-11 02:33:15 -0300

    feat: Adicionar controller de student a aplicação
    Issue: HotFix

--- sigtcc/sigtcc/api.py
+++ sigtcc/sigtcc/api.py
@@ -5,6 +5,7 @@ from academic.controllers import ExpertiseAreaController
 from academic.controllers import InstituteController
 from tcc.controllers import SupervisionRequestController
 from user.controllers import ProfessorController
+from user.controllers import StudentController
 from user.controllers import UserController
 
 docs = Swagger(settings={'docExpansion': 'none'})
@@ -13,5 +14,6 @@ api = NinjaExtraAPI(title='SIGTCC', version='1.0.0', docs=docs)
 api.register_controllers(ExpertiseAreaController)
 api.register_controllers(InstituteController)
 api.register_controllers(ProfessorController)
+api.register_controllers(StudentController)
 api.register_controllers(SupervisionRequestController)
 api.register_controllers(UserController)
