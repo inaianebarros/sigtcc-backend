4298816d1972ef6aa62841950c598bc1f9196a7e
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-07-11 02:32:19 -0300

    refactor: Atualizar controllers
    Issue: HotFix

--- sigtcc/tcc/controllers.py
+++ sigtcc/tcc/controllers.py
@@ -12,6 +12,7 @@ from tcc.schemas import ProfessorAnswerSupervisionRequestSchemaIn
 from tcc.schemas import StudentSupervisionRequestSchemaIn
 from tcc.schemas import StudentSupervisionRequestSchemaOut
 from user.models import ProfessorProfile
+from user.models import User
 
 
 @api_controller(
@@ -32,12 +33,18 @@ class SupervisionRequestController(ControllerBase):
     def answer_supervision_request(
         self, request: HttpRequest, professor_answer: ProfessorAnswerSupervisionRequestSchemaIn
     ):
+        breakpoint()
         if not (
             supervision_request := SupervisionRequest.objects.filter(
-                answer=SupervisionRequest.ANSWER.NO_ANSWER, uuid=professor_answer.uuid
+                uuid=professor_answer.uuid
             ).first()
         ):
             return status.HTTP_400_BAD_REQUEST, ReturnSchema(detail='Supervision request not found')
+
+        if supervision_request.answer != SupervisionRequest.ANSWER.NO_ANSWER:
+            return status.HTTP_400_BAD_REQUEST, ReturnSchema(
+                detail='Supervision request already answered'
+            )
 
         supervision_request.answer = professor_answer.answer
         supervision_request.professor_message = professor_answer.professor_message
@@ -63,30 +70,41 @@ class SupervisionRequestController(ControllerBase):
         },
     )
     def list_professor_supervisions_request(self, request: HttpRequest):
-        return SupervisionRequest.objects.filter(professor=request.user.professor_profiles.first())
+        return SupervisionRequest.objects.filter(
+            professor=request.user.professor_profiles.first(),
+            answer=SupervisionRequest.ANSWER.NO_ANSWER,
+        ).order_by('created_at')
 
     @route.post(
         '/student',
         response={
             status.HTTP_201_CREATED: StudentSupervisionRequestSchemaOut,
             status.HTTP_400_BAD_REQUEST: ReturnSchema,
+            status.HTTP_401_UNAUTHORIZED: ReturnSchema,
             status.HTTP_500_INTERNAL_SERVER_ERROR: ReturnSchema,
         },
     )
     def request_supervision(
         self, request: HttpRequest, student_request: StudentSupervisionRequestSchemaIn
     ):
+        if request.user.role == User.ROLE.PROFESSOR:
+            return status.HTTP_401_UNAUTHORIZED, ReturnSchema(
+                detail='Usuário professor não possui permissão para realizar requisição'
+            )
+
         if not (
             professor := ProfessorProfile.objects.filter(
                 uuid=student_request.professor_uuid
             ).first()
         ):
-            return status.HTTP_400_BAD_REQUEST, ReturnSchema(detail='Professor not found')
+            return status.HTTP_400_BAD_REQUEST, ReturnSchema(detail='Professor não encontrado')
 
         student = request.user.student_profiles.first()
 
         if SupervisionRequest.objects.filter(professor=professor, student=student).exists():
-            return status.HTTP_400_BAD_REQUEST, ReturnSchema(detail='Request already exists')
+            return status.HTTP_400_BAD_REQUEST, ReturnSchema(
+                detail='Aluno já fez solicitação para este professor'
+            )
 
         try:
             supervision_request = SupervisionRequest.objects.create(
