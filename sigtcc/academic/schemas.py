9caa1c2cc96647efc6fce7ec46166f74cd3dd637
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-07-11 02:29:17 -0300

    feat: Criar schema de CourseSchemaOut
    Issue: HotFix

--- sigtcc/academic/schemas.py
+++ sigtcc/academic/schemas.py
@@ -1,7 +1,14 @@
 from ninja import ModelSchema
 
+from academic.models import Course
 from academic.models import ExpertiseArea
 from academic.models import Institute
+
+
+class CourseSchemaOut(ModelSchema):
+    class Meta:
+        model = Course
+        fields = ['name']
 
 
 class ExpertiseAreaSchemaOut(ModelSchema):
